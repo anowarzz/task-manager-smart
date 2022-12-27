adcce3ea652fe8c77feb763575980f8f09d1731f
anowarzz <anowarhosen444@gmail.com>
2022-12-27 20:21:44 +0600

    navbar added primarily , need modification

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,20 +2,30 @@ import { createBrowserRouter } from "react-router-dom";
 import AddTask from "../components/Pages/AddTask/AddTask";
 import CompletedTask from "../components/Pages/CompletedTask/CompletedTask";
 import MyTasks from "../components/Pages/MyTasks/MyTasks";
+import Main from "../layout/Main";
 
 const router = createBrowserRouter([
-{
-    path: '/',
-    element: <AddTask />,
-},
-{
-    path: '/myTasks',
-    element: <MyTasks />
-},
-{
-    path: '/completedTasks',
-    element: <CompletedTask />
-}
+
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/addTask',
+                element: <AddTask />,
+            },
+            {
+                path: '/myTasks',
+                element: <MyTasks />
+            },
+            {
+                path: '/completedTasks',
+                element: <CompletedTask />
+            }
+        ]
+    }
+
+
 ])
 
 
