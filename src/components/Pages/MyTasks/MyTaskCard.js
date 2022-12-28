8a1819d37e9032698818713bccd7303a5d06bc39
anowarzz <anowarhosen444@gmail.com>
2022-12-28 21:57:16 +0600

    flowbite added to project

--- src/components/Pages/MyTasks/MyTaskCard.js
+++ src/components/Pages/MyTasks/MyTaskCard.js
@@ -2,27 +2,21 @@ import React from "react";
 
 const MyTaskCard = ({ task }) => {
   return (
- <div className="max-w-md rounded-md shadow-md bg-gray-50 text-gray-800">
-<img
-  src={task?.image}
-  alt=""
-  className="object-cover object-center w-full rounded-t-md h-40 bg-gray-500"
-/>
-<div className="flex flex-col items-center justify-between p-6 space-y-8">
-  <div className="space-y-2 self-start">
-    <h2 className="text-xl font-semibold tracking-wide">
-      {task?.title}
-    </h2>
-    <p className="text-gray-900"> {task?.description}</p>
-  </div>
-  <button
-    type="button"
-    className="flex items-center justify-center w-4/6 p-3 font-semibold tracking-wide rounded-md bg-violet-600 text-gray-50"
-  >
-    Read more
-  </button>
+<div className="max-w-lg shadow-2xl h-96 bg-gray-100 border-gray-700 rounded-md">
+
+    <div>
+        <img src={task?.image} alt="" className="h-52 w-full object-cover object-center" />
+    </div>
+
+    <div>
+        <h3 className="text-xl font-semibold text-gray-900 text-center mt-2">
+            {task?.title}
+        </h3>
+        <p className="text-gray-900 mt-2">
+    {task?.description}
+        </p>
+    </div>
 </div>
-</div> 
 
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
@@ -16,5 +16,5 @@ module.exports = {
       Oswald: [ 'Oswald', 'sans-serif']
     }
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
