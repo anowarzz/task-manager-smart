7d4f90cfc14cce904d6bf0dec983a3a1b5682b49
anowarzz <anowarhosen444@gmail.com>
2022-12-29 18:42:27 +0600

    footer added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+
 const Footer = () => {
     return (
         <div className='mt-12'>
-            <h2 className='text-center text-white'>First class footer coming soon</h2>
+
+
+        <hr className="my-6 border-gray-700 md:my-10 shadow-2xl" />
+
+        <div className="py-4">
+            <p className="text-white dark:text-gray-300 text-center"> 
+      © 2022 All Rights Reserved</p>
         </div>
+    </div>
+
+
     );
 };
 
